3619ae685d5febc8fcb2911a3613b441a211144f
Roberta Manuela <robertamanuellyaraujo@hotmail.com>
2019-08-21 13:25:58 -0300

    txt

---
+++ Lista_16/Ex01/viagem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex01
+{
+    class viagem
+    {
+        private double Distancia;
+        private double Tempo;
+        public void SetDistancia(double d) { Distancia = d; }
+        public void SetTempo(double t) { tempo = t; }
+        public double GetDistancia() { return Distancia; }
+        public double GetTempo() { return Tempo; }
+        public double CalcArea() { return 4 * Math.PI * raio * raio; }
+        public double CalcVolume() { return 4 * Math.PI * raio * raio * raio / 3; }
+    }
+}
---
+++ Lista_16/Ex02/Esfera.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex02
+{
+    class Esfera
+    {
+        private double raio;
+        public void SetRaio(double r) { raio = r; }
+        public double GetRaio() { return raio; }
+        public double CalcArea() { return 4 * Math.PI * raio * raio; }
+        public double CalcVolume() { return 4 * Math.PI * raio * raio * raio / 3; }
+    }
+}
+}
