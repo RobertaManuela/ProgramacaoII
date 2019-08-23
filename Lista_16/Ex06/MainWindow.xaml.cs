6dbe087053a1a4b3f2a8e42fc75d25dd94293b01
Roberta Manuela <robertamanuellyaraujo@hotmail.com>
2019-08-23 10:22:31 -0300

    Atte

--- Lista_16/Ex06/MainWindow.xaml.cs
+++ Lista_16/Ex06/MainWindow.xaml.cs
@@ -10,6 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
@@ -29,6 +41,22 @@ namespace Ex06
         {
             if (txt != null) txt.Text = slider.Value.ToString();
         }
+
+        private Bingo b;
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            b = new Bingo();
+            b.Iniciar(int.Parse(txt.Text));
+            txtNum.Text = "";
+            txtNums.Text = "";
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            int n = b.Proximo();
+            txtNum.Text = n.ToString();
+            txtNums.Text += n.ToString() + " ";
+        }
     }
 }
-
